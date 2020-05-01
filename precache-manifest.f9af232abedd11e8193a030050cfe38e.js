67ef5fd024ab02706da0eebb4dbb9b895faf4195
Julieth <juliethquinchia@gmail.com>
2020-05-01 17:44:43 -0500

    Updates

---
+++ precache-manifest.f9af232abedd11e8193a030050cfe38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d29c796ddf9d75d1b32a0087b5aa80e",
+    "url": "/subscritors-comono/index.html"
+  },
+  {
+    "revision": "66279df7c8d999c25d03",
+    "url": "/subscritors-comono/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d4d90c6adb96f54203a0",
+    "url": "/subscritors-comono/static/js/2.1700df8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/subscritors-comono/static/js/2.1700df8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66279df7c8d999c25d03",
+    "url": "/subscritors-comono/static/js/main.de77777f.chunk.js"
+  },
+  {
+    "revision": "2d8248edd5ec0ddc3dd4",
+    "url": "/subscritors-comono/static/js/runtime-main.8bb8c071.js"
+  }
+]);
