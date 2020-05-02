bc3d423c18a15af82e06c216d146421b12cafd20
Julieth <juliethquinchia@gmail.com>
2020-05-01 19:20:03 -0500

    test

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from './Table'
-import Sort from './Sort'
+
 
 const Fetch = () => {
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 
 import ReactDOM from 'react-dom';
 import './index.css';
-import React,{useState,useEffect} from 'react'
-import Table from './components/Table'
-import axios from 'axios'
 import Fetch from './components/Fetch';
-
-
-
 
 ReactDOM.render(
   <>
