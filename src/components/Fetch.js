13b55d3aa67cd5be73f5b515a4ebdd7a1e9b0502
Julieth <juliethquinchia@gmail.com>
2020-05-01 22:10:53 -0500

    test

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,46 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, {Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from './Table'
 
-
 const Fetch = () => {
 
-    /*Variable to save the sort data */
+    /*Variables to save the sort data */
     const [sortedName, setsortedName] = useState([])
     const [sortAge, setsortAge] = useState([])
     const [sortSport, setsortSport] = useState([])
 
     /* Functions to sort data */
-    const SortbyName = (data) => {
+    const Sort = (data,arg) => {
         setsortedName([])
-        setsortAge([])
         setsortSport([])
-        setsortedName(data.sort((a, b) => (a.name > b.name) ? 1 : -1))
-        return (<Table object={sortedName} />)
-    }
-
-    const SortbyAge = (data) => {
-        setsortedName([])
-        setsortAge([])
-        setsortSport([])
-
-        setsortAge(data.sort((a, b) => (a.age > b.age) ? 1 : -1))
-        return (<Table object={sortAge} />)
-    }
-    const SortbySport = (data) => {
-        setsortedName([])
-        setsortAge([])
-        setsortSport([])
-
-        setsortSport(data.sort((a, b) => (a.sport > b.sport) ? 1 : -1))
-        return (<Table object={sortSport} />)
-    }
-    /* Functions to sort data*/
+        setsortAge([])   
+        arg == 1 ? setsortedName(data.sort((a, b) =>  (a.name > b.name) ? 1 : -1)) :
+        arg==2?  setsortAge(data.sort((a, b) => (a.age > b.age) ? 1 : -1)) :
+        setsortSport(data.sort((a, b) => (a.sport > b.sport) ? 1 : -1));
+    }  
 
     /*Declare variable to save the data of fetch */
+
     const [subscritors, setSubscritors] = useState([]);
     const [loading, setLoading] = useState(true);
+
     /*We are telling React that the component has to do something after rendering*/
+
     useEffect(() => {
         axios.get('https://json-server-now-nine.now.sh/db.json/')
             .then(res => {
@@ -58,16 +43,17 @@ const Fetch = () => {
         return <h1>Loading....</h1>
     }
     return (
-        <>
+        <Fragment>
             <Table object={subscritors} />
             <div className="container mx-auto  pl-4 pr-4">
                 <div className="row">
-                    <button className=" col btn btn-info mx-1" onClick={() => SortbyName(subscritors)}>Sort by name </button>
-                    <button className="col btn btn-secondary mx-1" onClick={() => SortbyAge(subscritors)}>Sort by age </button>
-                    <button className="col btn btn-success mx-1" onClick={() => SortbySport(subscritors)}>Sort by sport </button>
+                    <button className=" col btn btn-info mx-1"onClick={()=>Sort(subscritors,1)}>Sort by name </button>
+                    <button className=" col btn btn-secondary mx-1 " onClick={() => Sort(subscritors,2)}>Sort by age </button>
+                    <button className=" col btn btn-success mx-1 " onClick={() => Sort(subscritors,3)}>Sort by sport </button>             
+
                 </div>
             </div>
-        </>
+        </Fragment>
     );
 }
 export default Fetch;
