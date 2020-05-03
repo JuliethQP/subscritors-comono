bfd5e375eb9d64909fd5135e27935ed00f021944
Julieth <juliethquinchia@gmail.com>
2020-05-02 20:25:12 -0500

    test

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -40,7 +40,13 @@ const Fetch = () => {
 
     if (loading === true){
         return (
-            <div className="d-flex justify-content-center mt-5">
+            <div className
+            
+            
+            
+            
+            
+            ="d-flex justify-content-center mt-5">
                 <div className="spinner-border" style={{ width: '5rem', height: '5rem' }} role="status">
                     <span className="sr-only">Loading...</span>
                 </div>
